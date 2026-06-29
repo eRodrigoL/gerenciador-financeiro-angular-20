4f2dd6effdef73a7af1445927fc5c10fbbadfe59
eRodrigoL <e.rodrigo.l@outlook.com>
2026-06-28 21:57:14 -0300

    🔧 chore(dados): Configura servidor de dados local
    - Removidos dados de transação hardcoded de `home.ts`
    - Criado `db.json` para armazenar

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -12,9 +12,5 @@ import { NoTransactions } from './components/no-transactions/no-transactions';
   styleUrl: './home.scss',
 })
 export class Home {
-  transactions = signal<Transaction[]>([
-    // { title: 'Salário', value: 100, type: TransactionType.INCOME },
-    // { title: 'VA', value: 50, type: TransactionType.INCOME },
-    // { title: 'Aluguel', value: 150, type: TransactionType.OUTCOME },
-  ]);
+  transactions = signal<Transaction[]>([]);
 }
