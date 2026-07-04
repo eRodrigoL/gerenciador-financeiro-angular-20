a38c6762a7b421676170a886e212b0055c9adbce
eRodrigoL <e.rodrigo.l@outlook.com>
2026-07-03 21:14:02 -0300

    ✨ feat(home): Adiciona busca de transações na inicialização
    - Implementa a interface `OnInit` do Angular.
    - Injeta `TransactionsService` para obter dados.
    - Carrega todas as transações ao iniciar o componente.
    - Atualiza o sinal `transactions` com os dados recebidos.

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,9 +1,9 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { Balance } from './components/balance/balance';
 import { TransactionItem } from './components/transaction-item/transaction-item';
 import { Transaction } from '../../shared/transaction/interfaces/transaction';
-import { TransactionType } from '../../shared/transaction/enums/transaction-type';
 import { NoTransactions } from './components/no-transactions/no-transactions';
+import { TransactionsService } from '../../shared/transaction/services/transactions';
 
 @Component({
   selector: 'app-home',
@@ -11,6 +11,20 @@ import { NoTransactions } from './components/no-transactions/no-transactions';
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home {
+export class Home implements OnInit {
+  private transactionsService = inject(TransactionsService);
+
   transactions = signal<Transaction[]>([]);
+
+  ngOnInit(): void {
+    this.getTransactions();
+  }
+
+  private getTransactions() {
+    this.transactionsService.getAll().subscribe({
+      next: (transactions) => {
+        this.transactions.set(transactions);
+      },
+    });
+  }
 }
