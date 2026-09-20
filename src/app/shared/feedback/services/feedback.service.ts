d1c4bdeeaec3baf8027f0871defa252ca2fa7f32
eRodrigoL <e.rodrigo.l@outlook.com>
2026-09-19 22:22:27 -0300

    feat: adiciona página de criação de transação

---
+++ src/app/shared/feedback/services/feedback.service.ts
@@ -0,0 +1,14 @@
+import { inject, Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FeedbackService {
+  private snackBar = inject(MatSnackBar);
+  success(message: string) {
+    this.snackBar.open(message, 'Ok', {
+      panelClass: 'snack-bar-feedback',
+    });
+  }
+}
